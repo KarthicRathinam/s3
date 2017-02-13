acceb1a4b368ec74d4992f6e33e68d82991ab2cc
KarthicRathinam <karthic.rathan@gmail.com>
2017-02-13 19:28:52 +0530

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             's3_file_test'
+maintainer       'test'
+maintainer_email 'bm@yahoo.in'
+license          'MIT'
+description      'Tests s3_file LWRP'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 's3_file'
