300ba7a254f1578c896153ad1ca2f3123980cfd4
KarthicRathinam <karthic.rathan@gmail.com>
2019-07-03 13:43:04 +0530

    rest

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             's3_file_test'
 maintainer       'test'
-maintainer_email 'bm@yahoo.in'
+maintainer_email 'bm@yahoo.co.in'
 license          'MIT'
 description      'Tests s3_file LWRP'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
